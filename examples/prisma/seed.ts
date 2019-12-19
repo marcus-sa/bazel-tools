5c36f39b97624d72b46900c79696b362d89dfc30
marcus-sa <marcus-sa@outlook.com>
2019-12-19 01:49:06 +0100

    fix(examples): prisma

--- examples/prisma/seed.ts
+++ examples/prisma/seed.ts
@@ -1,11 +1,33 @@
 import { Photon } from './index';
 
 (async() => {
+  // @ts-ignore
+  console.log(process.env);
+
+  const email = 'marcus-sa@outlook.com';
   const photon = new Photon();
+
   try {
     await photon.connect();
+
+    let user = await photon.users.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      user = await photon.users.create({
+        data: {
+          email,
+        },
+      });
+    }
+
+    console.log(user);
+
+    await photon.disconnect();
   } catch (err) {
-    // TODO:
     /*
       TODO: Figure out why this error occurs
       PhotonError: undefined target="exit" timestamp="2019-12-18T00:05:11.301Z" fields={"message":"1"}
